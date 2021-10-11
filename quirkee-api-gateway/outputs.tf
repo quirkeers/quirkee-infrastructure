a3fd4641302d7d7f09d2375d6efb97d04d4cb059
quirkee.admin <quirkee.group@gmail.com>
2021-10-11 11:09:03 +0800

    add api endpoint

--- quirkee-api-gateway/outputs.tf
+++ quirkee-api-gateway/outputs.tf
@@ -1,3 +1,7 @@
 output "api_gateway_api_execution_arn" {
   value = module.api_gateway.apigatewayv2_api_execution_arn
 }
+
+output "api_gateway_api_endpoint" {
+  value = module.api_gateway.apigatewayv2_api_api_endpoint
+}
