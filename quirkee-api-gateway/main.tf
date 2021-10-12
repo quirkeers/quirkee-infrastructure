b5ee68a1a15c49e494424b0f43258a87a88e5426
quirkee.admin <quirkee.group@gmail.com>
2021-10-12 20:57:51 +0800

    try add route 53 config

--- quirkee-api-gateway/main.tf
+++ quirkee-api-gateway/main.tf
@@ -1,3 +1,15 @@
+variable "subdomain_map" {
+  default = {
+    "production": "www",
+    "development": "dev"
+  }
+}
+
+locals  {
+  domain_name = "quirkee.net"
+  subdomain = lookup(var.subdomain_map, var.env)
+}
+
 module "api_gateway" {
   source = "terraform-aws-modules/apigateway-v2/aws"
 
@@ -10,17 +22,39 @@ module "api_gateway" {
     allow_origins = ["*"]
   }
 
-  create_api_domain_name = false
+  domain_name = local.domain_name
+  domain_name_certificate_arn = module.acm.acm_certificate_arn
 
   integrations = {
-    "ANY /" = {
+    "ANY /${var.name}" = {
       lambda_arn             = var.lambda_arn
       payload_format_version = "2.0"
       timeout_milliseconds   = 12000
     }
-
-    "$default" = {
-      lambda_arn = var.lambda_arn
-    }
   }
+}
+
+data "aws_route53_zone" "this" {
+  name = local.domain_name
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = local.subdomain
+  type    = "A"
+
+  alias {
+    name                   = module.api_gateway.apigatewayv2_domain_name_configuration[0].target_domain_name
+    zone_id                = module.api_gateway.apigatewayv2_domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 3.0"
+
+  domain_name               = local.domain_name
+  zone_id                   = data.aws_route53_zone.this.id
+  subject_alternative_names = ["${local.subdomain}.${local.domain_name}"]
 }
