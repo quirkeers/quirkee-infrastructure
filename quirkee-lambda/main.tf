9017ac5eb28c4f1bb662c6efe59176f5aeefc7fb
quirkee.admin <quirkee.group@gmail.com>
2021-10-12 16:31:02 +0800

    remove layer

--- quirkee-lambda/main.tf
+++ quirkee-lambda/main.tf
@@ -7,27 +7,27 @@ module "lambda_function" {
   handler = "index.main"
   source_path = [
     var.handler_path,
-//    {
-//      path     = var.node_project_path,
-//      commands = [
-//        "npm install",
-//        ":zip"
-//      ],
-//      patterns = [
-//        "!.*/.*\\.txt",    # Skip all txt files recursively
-//        "node_modules/.+", # Include all node_modules
-//        "!node_modules/aws-sdk/.+", # Exclude all node_modules/aws-sdk
-//      ],
-//    }
+    {
+      path     = var.node_project_path,
+      commands = [
+        "npm install",
+        ":zip"
+      ],
+      patterns = [
+        "!.*/.*\\.txt",    # Skip all txt files recursively
+        "node_modules/.+", # Include all node_modules
+        "!node_modules/aws-sdk/.+", # Exclude all node_modules/aws-sdk
+      ],
+    }
   ]
 
   # store lambda in S3
   store_on_s3 = true
   s3_bucket = var.s3_bucket
 
-  layers = [
-    module.lambda_layer_s3.lambda_layer_arn,
-  ]
+//  layers = [
+//    module.lambda_layer_s3.lambda_layer_arn,
+//  ]
 
   allowed_triggers = {
     AllowExecutionFromAPIGateway = {
@@ -37,17 +37,17 @@ module "lambda_function" {
   }
 }
 
-module "lambda_layer_s3" {
-  source = "terraform-aws-modules/lambda/aws"
-
-  create_layer = true
-
-  layer_name          = "${var.env}-${var.name}-layer"
-  description         = "${var.env}-${var.name}'s common layer"
-  compatible_runtimes = ["nodejs14.x"]
-
-  source_path = var.node_project_path
-
-  store_on_s3 = true
-  s3_bucket   = var.s3_bucket
-}
+//module "lambda_layer_s3" {
+//  source = "terraform-aws-modules/lambda/aws"
+//
+//  create_layer = true
+//
+//  layer_name          = "${var.env}-${var.name}-layer"
+//  description         = "${var.env}-${var.name}'s common layer"
+//  compatible_runtimes = ["nodejs14.x"]
+//
+//  source_path = var.node_project_path
+//
+//  store_on_s3 = true
+//  s3_bucket   = var.s3_bucket
+//}
