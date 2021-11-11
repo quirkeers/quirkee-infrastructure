266d212301957aa2279ac80c9b69b01f97bb66f7
quirkee.admin <quirkee.group@gmail.com>
2021-11-11 21:35:31 +0800

    add var config

--- terraform.tf
+++ terraform.tf
@@ -19,6 +19,10 @@ module "quirkee_lambda" {
   node_project_path = var.node_project_path
   s3_bucket = module.quirkee_lambda_s3_bucket.app_bucket_id
 
+  SERVICE_TO_SERVICE_OAUTH_CLIENT_URI =  var.SERVICE_TO_SERVICE_OAUTH_CLIENT_URI
+  SERVICE_TO_SERVICE_OAUTH_CLIENT_ID =  var.SERVICE_TO_SERVICE_OAUTH_CLIENT_ID
+  SERVICE_TO_SERVICE_OAUTH_CLIENT_SECRET =  var.SERVICE_TO_SERVICE_OAUTH_CLIENT_SECRET
+
   attach_policy_jsons = var.attach_policy_jsons
   policy_jsons = var.policy_jsons
   number_of_policy_jsons =  var.number_of_policy_jsons
